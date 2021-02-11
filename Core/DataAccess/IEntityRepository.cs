c5da74ebe5dc753b705a3af090816c6a4e79282d
Mustafa <gkmstf29@gmail.com>
2021-02-11 14:57:57 +0300

    FinalProject

---
+++ Core/DataAccess/IEntityRepository.cs
@@ -0,0 +1,22 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Core.DataAccess
+{
+    //generic constraint
+    //class : reference type
+    //new() : new'lenebilir olmalı yani IEntity olmaz onu implemente edenler olur
+    //yalnızca referans tip ve IEntity(implemente eden) olabilir
+    public interface IEntityRepository<T> where T:class,IEntity,new()
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        
+    }
+}
