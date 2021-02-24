bac92b5c9e33fea02e458dbdeb898208b7036bc0
Mustafa <gkmstf29@gmail.com>
2021-02-24 18:54:46 +0300

    Final Project

--- Bussiness/Abstract/IProductService.cs
+++ Bussiness/Abstract/IProductService.cs
@@ -1,4 +1,5 @@
-﻿using Entities.Concrete;
+﻿using Core.Utilities.Results;
+using Entities.Concrete;
 using Entities.DTOs;
 using System;
 using System.Collections.Generic;
@@ -8,10 +9,13 @@ namespace Bussiness.Abstract
 {
     public interface IProductService
     {
-        List<Product> GetAll();
-        List<Product> GetAllByCategoryId(int id);
-        List<Product> GetByUnitPrice(decimal min, decimal max);
+        IDataResult<List<Product>> GetAll();
+        IDataResult<List<Product>> GetAllByCategoryId(int id);
+        IDataResult<List<Product>> GetByUnitPrice(decimal min, decimal max);
+        IDataResult<Product> GetById(int productId);
 
-        List<ProductDetailDto> GetProductDetails();
+        IDataResult<List<ProductDetailDto>> GetProductDetails();
+        IResult Add(Product product);
+
     }
 }
---
+++ Bussiness/Constants/Messages.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bussiness.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Ürün eklendi";
+
+        public static string ProductNameInvalid = "Ürün ismi geçersiz";
+
+        public static string MaintenanceTime = "Sistem bakımda";
+
+        public static string ProductListed = "Ürünler listelendi";
+    }
+}
